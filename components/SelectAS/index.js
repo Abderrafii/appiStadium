7d34ffd0dfd16339757ca8bd2877a1446155640f
AbderrafiiG <goutaibabderrafii@gmail.com>
2022-06-02 23:56:24 +0100

    select

--- components/SelectAS/index.js
+++ components/SelectAS/index.js
@@ -1,34 +1,42 @@
-import { Select } from 'antd';
+import { Button, Select } from 'antd';
 import React from 'react';
 import styled from 'styled-components';
 
-const { Option } = Select;
+const StyledSelect = styled(Select)`
+  .ant-input,
+  .ant-select-selector {
+    border-radius: 7px !important;
+    border: 0.5px solid gray !important;
 
+    &:focus,
+    &:active &:focus-visible {
+      outline: none !important;
+      box-shadow: none !important;
+      border: 0 !important;
+      border-bottom: 2px solid #e0e0e0 !important;
+    }
+    .ant-select-selection-item {
+      padding-bottom: 20px !important;
+    }
+  }
+`;
 const SelectAS = ({ selectProps, optionsProps, options, defaultValue }) => {
-  const StyledSelect = styled(Select)`
-    .ant-select:not(.ant-select-customize-input) .ant-select-selector {
-      border-radius: 10px;
-    }
-    .ant-select-selection {
-      background-color: green;
-      border-radius: '24px';
-    }
-  `;
   return (
     <StyledSelect
-      style={{ width: 450, borderRadius: '20px', margin: 3 }}
+      size='large'
+      style={{ width: 450, margin: 3 }}
       dropdownClassName=' rounded-b-lg'
       //   style={{ width: '100%', borderTopLeftRadius: 25 }}
       {...selectProps}
       defaultValue={defaultValue}>
       {options.map((option, key) => (
-        <Option
+        <Select.Option
           key={key}
           value={option.value}
           className='bg-white'
           {...optionsProps}>
           {option.label}
-        </Option>
+        </Select.Option>
       ))}
     </StyledSelect>
   );
