fcac9d6dc2e2e2f23a8a205366c7f5d6840cca7e
AissamJ <jadliaissam@gmail.com>
2022-06-02 17:29:46 +0100

    users add  +edit linked and ant design

--- context/myContext.js
+++ context/myContext.js
@@ -1,5 +1,7 @@
-import { createContext } from "react";
+import {createContext} from "react";
 
 export const authContext = createContext();
+
+export const LoadContext = createContext();
 
 
--- pages/users/index.js
+++ pages/users/index.js
@@ -1,16 +1,35 @@
 import {useEffect, useState} from "react";
 import {getSystemUsers} from "../../config/apis";
-import {Table} from "antd";
-import Loader from "../../components/Loader";
+import {Avatar, Button, Table} from "antd";
 import Link from "next/link";
+import Loader from "../../components/Loader";
+import {SearchOutlined, EditOutlined} from '@ant-design/icons';
+import {useRouter} from "next/router";
 
 const UsersList = ({}) => {
     const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true)
+    const router = useRouter();
+    const colors = ['#f56a00', '#7265e6', '#ffbf00', '#00a2ae'];
+    const gerRandomColor = () => {
+        return colors[Math.floor(Math.random() * colors.length)];
+    }
 
     const columns = [
-        {title: '# ID', dataIndex: '_id', key: '_id'},
+        {
+            title: '',
+            dataIndex: 'first_name',
+            key: 'image',
+            render: (text, record) => (
+                <Avatar
+                    size={"large"}
+                    style={{color: '#ffffff', backgroundColor: gerRandomColor()}}>
+                    {text[0]}
+                </Avatar>
+            ),
+            width: 75,
+        },
         {title: 'First Name', dataIndex: 'first_name', key: 'first_name'},
         {title: 'Last Name', dataIndex: 'last_name', key: 'last_name'},
         {
@@ -38,36 +57,34 @@ const UsersList = ({}) => {
             key: 'actions',
             dataIndex: '_id',
             render: (_id) => (
-                <div>
-                    <Link href={`/users/${_id}`}>Details</Link>
-                    <Link href={`/users/${_id}/edit`}>Edit</Link>
-                </div>
+                <>
+                    <Button icon={<SearchOutlined/>} type="primary" onClick={()=>router.push(`/users/${_id}`)}> Details</Button>
+                    <Button icon={<EditOutlined/>} type="secondary" onClick={()=>router.push(`/users/${_id}/edit`)}> Edit</Button>
+                </>
             ),
         },
     ];
 
     const getUsers = async () => {
-        setLoading(true);
         try {
             const response = await getSystemUsers();
             setUsers(response.data);
         } catch (error) {
             setError(error);
         }
-        setLoading(false);
     };
 
     useEffect(() => {
+        setLoading(true);
         getUsers().catch(error => {
             setError(error);
-        });
+        }).finally(() => {
+            setLoading(false);
+        })
     }, []);
 
-    if (loading)
-        return <Loader/>;
-
     return <div className={""}>
-        {loading ? "Loading ... " : <Table dataSource={users} columns={columns}/>};
+        {loading ? <Loader/> : <Table dataSource={users} columns={columns}/>};
     </div>
 }
 
